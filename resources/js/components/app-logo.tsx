64999031d8f9003a5b4495626d477ede138d8367
Marcel-wil <wilczynskimarceli@gmail.com>
2025-08-06 12:09:34 +0200

    name change

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -3,8 +3,8 @@ import { Link } from '@inertiajs/react';
 export default function AppLogo() {
     return (
         <Link href="/" className="block">
-            <div className="grid flex-1 ml-1 text-sm text-left">
-                <span className="mb-0.5 truncate leading-tight font-semibold">RijexamenMeldingen</span>
+            <div className="ml-1 grid flex-1 text-left text-sm">
+                <span className="mb-0.5 truncate leading-tight font-semibold">Rijbewijsboeker</span>
             </div>
         </Link>
     );
